a571999e1a119952a3e6e7bd489998f43a27c2cb
Maksim Timofeyev <makstim@list.ru>
2025-08-06 21:41:57 +0300

    Commit 1

--- app/src/main/java/ru/netology/yandexmap/MainActivity.kt
+++ app/src/main/java/ru/netology/yandexmap/MainActivity.kt
@@ -11,37 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.yandex.mapkit.MapKitFactory
 import ru.netology.yandexmap.ui.theme.YandexMapTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MapKitFactory.setApiKey("669a46a5-1e74-414f-891d-4656f092498a")
         enableEdgeToEdge()
-        setContent {
-            YandexMapTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+
+
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    YandexMapTheme {
-        Greeting("Android")
-    }
-}
